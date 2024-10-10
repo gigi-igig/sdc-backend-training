06fd851a1d2b06c4e787dfe7a444e73fadfb62b1
gigi-igig <73539040+gigi-igig@users.noreply.github.com>
2024-10-10 12:57:32 +0800

    Add files via upload
    hw3_2

--- main.py
+++ main.py
@@ -29,17 +29,16 @@ async def read_item(
     
     return response
 
-
 @app.put("/items/{item_id}")
 async def update_item(
                       item_id: Annotated[Decimal, Path(gt=1, lt=1000, description="Item ID must be between 1 and 1000.")], 
-                      item: Item, 
+                      item: Item = None, 
                       q: Annotated[str | None, Query(min_length=3, max_length=50, description="Query 'q' must be between 3 and 50 characters.")] = None
                       ):
     updated_item = item.model_dump()
     response = {"item_id": item_id, **updated_item}
 
     if q:
-        response["q"] = q
+        response.update({"q": q})
     
     return response
